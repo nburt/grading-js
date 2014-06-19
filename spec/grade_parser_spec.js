ae6016d3db9bb0777ab4ffc698f77d7e318a7810
Nathanael Burt <nathanael.burt@gmail.com>
2014-06-19 07:19:10 -0600

    function to return whether a not a grade went down or up from the previous grade
    another funciton to determine if a students grades are trending downward

--- spec/grade_parser_spec.js
+++ spec/grade_parser_spec.js
@@ -1,8 +1,38 @@
-describe("GradeParser", function() {
+describe("GradeParser", function () {
 
-  it("returns an array of up/down/even for the given array", function() {
-    var result = GradeParser.getMovements([6,3,5,4,3,4,4,5]);
-    expect(result).toEqual(["down","up","down","down","up","even","up"]);
+  it("returns an array of up/down/even for the given array", function () {
+    var result = GradeParser.getMovements([6, 3, 5, 4, 3, 4, 4, 5]);
+    expect(result).toEqual(["down", "up", "down", "down", "up", "even", "up"]);
+  });
+
+  it("can tell if a student is in decline", function () {
+    var result = GradeParser.getTrend([10, 10, 10, 9, 9, 8, 8, 8, 8, 7]);
+    expect(result).toEqual("in decline");
+  });
+
+  it("tests another case of a student in decline", function () {
+    var result = GradeParser.getTrend([10, 9, 8, 7]);
+    expect(result).toEqual("in decline");
+  });
+
+  it("tests another case of a student in decline", function () {
+    var result = GradeParser.getTrend([10, 9, 8, 7, 8, 7, 6, 5]);
+    expect(result).toEqual("in decline");
+  });
+
+  it("can tell if a student is not in decline", function () {
+    var result = GradeParser.getTrend([10, 9, 8, 7, 8, 7, 6 ]);
+    expect(result).toEqual("not in decline");
+  });
+
+  it("tests another case of a student who is not in decline", function () {
+    var result = GradeParser.getTrend([10, 9, 8, 8, 8, 8]);
+    expect(result).toEqual("not in decline");
+  });
+
+  it("tests another case of a student who is not in decline", function () {
+    var result = GradeParser.getTrend([10, 11, 12, 13]);
+    expect(result).toEqual("not in decline");
   });
 
 });
--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,7 +1,34 @@
 window.GradeParser = {
 
   getMovements: function (grades) {
-    return [];
-  }
+    var array = [];
+    for (i = 0; i < grades.length; i++) {
+      if (grades[i] < grades[i - 1]) {
+        array.push('down');
+      } else if (grades[i] > grades[i - 1]) {
+        array.push('up');
+      } else {
+        array.push('even')
+      }
+    }
+    array.shift();
+    return array;
+  },
 
+  getTrend: function (grades) {
+    var movements = GradeParser.getMovements(grades);
+    var consecutive_decline = 0;
+    for (i = 0; i < movements.length; i++) {
+      if (movements[i] === 'up') {
+        consecutive_decline = 0;
+      } else if (movements[i] === 'down') {
+        consecutive_decline += 1;
+      }
+    }
+    if (consecutive_decline > 2) {
+      return 'in decline';
+    } else {
+      return 'not in decline';
+    }
+  }
 };
